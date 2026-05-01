e6089f1c857f1fd4c3050b300219f9f714b0d49a
nad014 <c243434@ugrad.iiuc.ac.bd>
2026-05-01 23:08:28 +0600

    Create Rabin_karp.cpp

---
+++ 0/Rabin_karp.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string text, pattern;
+
+    cout<<"Enter text: ";
+    cin>>text;
+
+    cout<<"Enter pattern: ";
+    cin>>pattern;
+
+    int n=text.length();
+    int m=pattern.length();
+
+    for(int i=0;i<=n-m;i++)
+    {
+        int j;
+
+        for(j=0;j<m;j++)
+        {
+            if(text[i+j]!=pattern[j])
+                break;
+        }
+
+        if(j==m)
+            cout<<"Pattern found at index "<<i<<endl;
+    }
+
+    return 0;
+}
